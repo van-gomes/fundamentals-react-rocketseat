02fa4338df5317e9be952a6954af3e5e5d0a336e
van-gomes <nninha.gomes@gmail.com>
2024-07-31 19:08:15 -0300

    Style module css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { Header } from "./components/Header";
 import { Post } from "./Post";
 
 export function App() {
   return (
     <>
+      <Header />
       <Post
         author="Vânia Gomes"
         content="Conteúdo do post"
