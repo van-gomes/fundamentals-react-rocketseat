020fa0957c555ffc8e70328994a02b7c5694b2c6
van-gomes <nninha.gomes@gmail.com>
2024-08-04 18:11:52 -0300

    Create Sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,27 @@ import { Header } from "./components/Header";
 import { Post } from "./Post";
 import "./global.css";
 import styles from './App.module.css';
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <>
       <Header />
-      <Post
+
+      <div className={styles.wrapper}>
+        <aside>
+          <Sidebar/>
+        </aside>
+
+        <Post
         author="Vânia Gomes"
         content="Conteúdo do post"
-      />
+        />
+        <Post
+          author="Vânia Gomes"
+          content="Conteúdo do post"
+        />
+      </div>
     </>
   )
 }
