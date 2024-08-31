09ad461b704bd558c6113d840a8b4b123ad7a521
van-gomes <nninha.gomes@gmail.com>
2024-08-31 17:09:34 -0300

    Build Comments Form

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -21,6 +21,18 @@ export function Post() {
         <p>👉<a href="">jane.design/doctorcare</a></p>
         <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea
+          placeholder="Deixe um comentário"
+        />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   )
 }
