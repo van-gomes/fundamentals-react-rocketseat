af359c0a57aea9a6228321e3fb0beac537ee1e00
van-gomes <nninha.gomes@gmail.com>
2024-09-01 20:08:26 -0300

    Build Avatar Component

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
 import { ThumbsUp, Trash } from 'phosphor-react';
@@ -5,8 +6,8 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <img src="https://github.com/diego3g.png" alt="" />
-
+      <Avatar hasBorder={false} src="https://github.com/diego3g.png" alt="" />
+    
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
           <header>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { PencilLine } from 'phosphor-react'
 
 import styles from "./Sidebar.module.css";
+import { Avatar } from './Avatar';
 
 export function Sidebar() {
     return (
@@ -11,8 +12,7 @@ export function Sidebar() {
             />
 
             <div className={styles.profile}>
-                <img className={styles.avatar} 
-                    src="https://github.com/diego3g.png" />
+                <Avatar src="https://github.com/diego3g.png" />
                 <strong>Vânia Gomes</strong>
                 <span>Web Developer</span>
             </div>
