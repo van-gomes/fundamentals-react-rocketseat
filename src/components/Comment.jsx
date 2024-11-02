4eaa71269b8748d0722a5d695877ad4fcc8cac76
van-gomes <nninha.gomes@gmail.com>
2024-11-02 19:01:09 -0300

    Like comments

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,13 +1,20 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-undef */
+import { useState } from 'react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
 import { ThumbsUp, Trash } from 'phosphor-react';
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
     onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1);
   }
 
   return (
@@ -31,9 +38,9 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
